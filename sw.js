2326de2b5d2faefb22ccd5ae22a5f7b754e69512
angelmicelti <angelmicelti@users.noreply.github.com>
2026-06-01 22:03:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - ExpoVilladiego PWA
-const CACHE_NAME = 'expovilladiego-v3.98';
+const CACHE_NAME = 'expovilladiego-v3.99';
 
 // Assets estáticos a pre-cachear durante la instalación
 const STATIC_ASSETS = [
